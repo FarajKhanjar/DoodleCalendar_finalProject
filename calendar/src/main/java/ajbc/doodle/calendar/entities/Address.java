948cf353be1929c98f2d620dc58791e39b05cc90
FarajKhanjar <farajkhanjar@gmail.com>
2022-07-05 00:25:39 +0300

    Create Address class.

---
+++ calendar/src/main/java/ajbc/doodle/calendar/entities/Address.java
@@ -0,0 +1,29 @@
+package ajbc.doodle.calendar.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "addresses")
+public class Address {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String country;
+	private String city;
+	private String street;
+	private int number;
+
+}
