e9ab8ec328d6448d5add1e1018e6b1d7fb286bc8
FarajKhanjar <farajkhanjar@gmail.com>
2022-07-04 16:49:07 +0300

    Preparing project and deleting files from the template project.

--- calendar/src/main/java/ajbc/doodle/calendar/AppConfig.java
+++ calendar/src/main/java/ajbc/doodle/calendar/AppConfig.java
@@ -3,9 +3,6 @@ package ajbc.doodle.calendar;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import ajbc.doodle.calendar.entities.Category;
-import ajbc.doodle.calendar.entities.Product;
-import ajbc.doodle.calendar.entities.Supplier;
 
 import java.util.Properties;
 
@@ -30,66 +27,67 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 public class AppConfig {
 
-	private static final int INIT_SIZE = 10, MAX_SIZE = 100, MAX_WAIT = 500, MAX_IDLE = 50, MIN_IDLE = 2;
-	@Value("${user}")
-	private String user;
-	@Value("${password}")
-	private String password;
-	@Value("${server_address}")
-	private String serverAddress;
-	@Value("${port}")
-	private String port;
-	@Value("${db_name}")
-	private String databaseName;
-	@Value("${server_name}")
-	private String serverName;
-	@Value("${driver_class_name}")
-	private String driverClassName;
+	//private static final int INIT_SIZE = 10, MAX_SIZE = 100, MAX_WAIT = 500, MAX_IDLE = 50, MIN_IDLE = 2;
+	
+//	@Value("${user}")
+//	private String user;
+//	@Value("${password}")
+//	private String password;
+//	@Value("${server_address}")
+//	private String serverAddress;
+//	@Value("${port}")
+//	private String port;
+//	@Value("${db_name}")
+//	private String databaseName;
+//	@Value("${server_name}")
+//	private String serverName;
+//	@Value("${driver_class_name}")
+//	private String driverClassName;
 	
 
-	@Bean
-	public DataSource dataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setUrl(createUrl());
-		dataSource.setUsername(user);
-		dataSource.setPassword(password);
-		dataSource.setDriverClassName(driverClassName);
-		
-		return dataSource;
-	}
-	
-	
-	@Bean
-	public LocalSessionFactoryBean sessionFactory(DataSource  dataSource) {
-		LocalSessionFactoryBean factory = new LocalSessionFactoryBean();
-		factory.setDataSource(dataSource);
-		factory.setAnnotatedClasses(Category.class, Supplier.class, Product.class);
-		
-		Properties props = new Properties();
-		props.setProperty("hibernate.dialect", "org.hibernate.dialect.SQLServerDialect");
-		props.setProperty("hibernate.show_sql", "false");
-		props.setProperty("hibernate.format_sql", "true");
-		
-		factory.setHibernateProperties(props);
-		return factory;
-	}
-	
-	
-	
-	@Bean
-	public HibernateTemplate hibernateTemplate(SessionFactory sessionFactory) {
-		return new HibernateTemplate(sessionFactory);
-	}
-	
-	
-	@Bean
-	public HibernateTransactionManager txMgr(SessionFactory sessionFactory) {
-		return new HibernateTransactionManager(sessionFactory);
-	}
-	
-	
-	private String createUrl() {
-		return "jdbc:sqlserver://" + serverAddress + ":" + port + ";databaseName=" + databaseName + ";servername="
-				+ serverName + ";" + ";encrypt=false";
-	}
+//	@Bean
+//	public DataSource dataSource() {
+//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//		dataSource.setUrl(createUrl());
+//		dataSource.setUsername(user);
+//		dataSource.setPassword(password);
+//		dataSource.setDriverClassName(driverClassName);
+//		
+//		return dataSource;
+//	}
+//	
+//	
+//	@Bean
+//	public LocalSessionFactoryBean sessionFactory(DataSource  dataSource) {
+//		LocalSessionFactoryBean factory = new LocalSessionFactoryBean();
+//		factory.setDataSource(dataSource);
+//		factory.setAnnotatedClasses(Category.class, Supplier.class, Product.class);
+//		
+//		Properties props = new Properties();
+//		props.setProperty("hibernate.dialect", "org.hibernate.dialect.SQLServerDialect");
+//		props.setProperty("hibernate.show_sql", "false");
+//		props.setProperty("hibernate.format_sql", "true");
+//		
+//		factory.setHibernateProperties(props);
+//		return factory;
+//	}
+//	
+//	
+//	
+//	@Bean
+//	public HibernateTemplate hibernateTemplate(SessionFactory sessionFactory) {
+//		return new HibernateTemplate(sessionFactory);
+//	}
+//	
+//	
+//	@Bean
+//	public HibernateTransactionManager txMgr(SessionFactory sessionFactory) {
+//		return new HibernateTransactionManager(sessionFactory);
+//	}
+//	
+//	
+//	private String createUrl() {
+//		return "jdbc:sqlserver://" + serverAddress + ":" + port + ";databaseName=" + databaseName + ";servername="
+//				+ serverName + ";" + ";encrypt=false";
+//	}
 }
