e601adee1bc225916af876b11a12465dcb420a4f
FarajKhanjar <farajkhanjar@gmail.com>
2022-07-12 15:03:33 +0300

    Update notification manager, pushing multi-messages works.

--- calendar/src/main/java/ajbc/doodle/calendar/controllers/NotificationController.java
+++ calendar/src/main/java/ajbc/doodle/calendar/controllers/NotificationController.java
@@ -152,104 +152,13 @@ public class NotificationController {
 	public String publicSigningKeyBase64() {
 		return dataManager.getServerKeys().getPublicKeyBase64();
 	}
-
-	@PostConstruct
-	public void update() throws DaoException {
-		notificationManager.setManagerData(dataManager);
+	
+	@Scheduled(initialDelay = 2000 ,fixedDelay = 6000)
+	public void run() throws DaoException {
+		notificationManager.setDataManager(dataManager);
 		List<Notification> notifications = notificationDao.getAllNotifications();
 		notificationManager.addNotification(notifications);
-	}
-	
-	@Scheduled(fixedDelay = 3_000)
-	public void testNotification() throws DaoException {
-		List<User> users = userDao.getAllUsers();
-		List<Notification> notifications = notificationDao.getAllNotifications();
-		Object message = new PushMessage(notifications.get(1).getTitle(), notifications.get(1).getMessage());
-
-		users.forEach(oneUser -> {
-			if (oneUser.getUserOnline()==1) {
-				byte[] result;
-				try {
-					result = dataManager.getCryptoService().encrypt(dataManager.getObjectMapper().writeValueAsString(message),
-							oneUser.getSubscriptionInfo().getPublicKey(),
-							oneUser.getSubscriptionInfo().getAuthKey(), 0);
-					sendPushMessage(oneUser.getSubscriptionInfo().getEndpoint(), result);
-					
-				} catch (InvalidKeyException | NoSuchAlgorithmException | InvalidKeySpecException
-						| InvalidAlgorithmParameterException | NoSuchPaddingException | IllegalBlockSizeException
-						| BadPaddingException | JsonProcessingException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
-			}
-		});
-	}
-
-//	/**
-//	 * @return true if the subscription is no longer valid and can be removed, false
-//	 *         if everything is okay
-//	 */
-	private boolean sendPushMessage(String endPoint, byte[] body) {
-		String origin = null;
-		try {
-			URL url = new URL(endPoint);
-			origin = url.getProtocol() + "://" + url.getHost();
-			
-		} catch (MalformedURLException e) {
-			Application.logger.error("create origin", e);
-			return true;
-		}
-
-		Date today = new Date();
-		Date expires = new Date(today.getTime() + 12 * 60 * 60 * 1000);
-
-		String token = JWT.create().withAudience(origin).withExpiresAt(expires)
-				.withSubject("mailto:example@example.com").sign(dataManager.getJwtAlgorithm());
-
-		URI endpointURI = URI.create(endPoint);
-
-		Builder httpRequestBuilder = HttpRequest.newBuilder();
-		if (body != null) {
-			httpRequestBuilder.POST(BodyPublishers.ofByteArray(body)).header("Content-Type", "application/octet-stream")
-					.header("Content-Encoding", "aes128gcm");
-		} else {
-			httpRequestBuilder.POST(BodyPublishers.ofString("trytrytry"));
-			// httpRequestBuilder.header("Content-Length", "0");
-		}
-
-		HttpRequest request = httpRequestBuilder.uri(endpointURI).header("TTL", "180")
-				.header("Authorization", "vapid t=" + token + ", k=" + dataManager.getServerKeys().getPublicKeyBase64()).build();
-		try {
-			HttpResponse<Void> response = dataManager.getHttpClient().send(request, BodyHandlers.discarding());
-
-			switch (response.statusCode()) {
-			case 201:
-				Application.logger.info("Push message successfully sent: {}", endPoint);
-				break;
-			case 404:
-			case 410:
-				Application.logger.warn("Subscription not found or gone: {}", endPoint);
-				// remove subscription from our collection of subscriptions
-				return true;
-			case 429:
-				Application.logger.error("Too many requests: {}", request);
-				break;
-			case 400:
-				Application.logger.error("Invalid request: {}", request);
-				break;
-			case 413:
-				Application.logger.error("Payload size too large: {}", request);
-				break;
-			default:
-				Application.logger.error("Unhandled status code: {} / {}", response.statusCode(), request);
-			}
-			
-		} catch (IOException | InterruptedException e) {
-			Application.logger.error("send push message", e);
-		}
-		
-		return false;
+		notificationManager.run();
 	}
 
 }
