33e64c5804df4e435eab2513dcc7455fde548ecd
FarajKhanjar <farajkhanjar@gmail.com>
2022-07-14 00:50:08 +0300

    Add NotificationManager tests.

--- calendar/src/test/java/ajbc/doodle/calendar/NotificationManagerTest.java
+++ calendar/src/test/java/ajbc/doodle/calendar/NotificationManagerTest.java
@@ -4,13 +4,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import ajbc.doodle.calendar.daos.DaoException;
+import ajbc.doodle.calendar.entities.Notification;
+import ajbc.doodle.calendar.enums.Unit;
 import ajbc.doodle.calendar.managers.NotificationManager;
 
 class NotificationManagerTest {
 
 	private NotificationManager manager;
 	
-	public NotificationManagerTest() {
+	Notification testNotification1;
+	Notification testNotification2;
+	
+	public NotificationManagerTest() throws DaoException {
 		
 		this.manager = new NotificationManager();
 	}
@@ -21,5 +27,32 @@ class NotificationManagerTest {
 		assertNotNull(manager);
 		assertNotNull(manager.getQueue());
 	}
-
+	
+	@Test
+	public void addNotificationTest() throws DaoException
+	{
+		testNotification1 = new Notification("AJBC", "This is a reminder message.",
+				Unit.MINUTES, 10 ,null,null);
+		
+		manager.getQueue().add(testNotification1);
+		assertTrue(manager.getQueue().size()==1);	
+		assertEquals(testNotification1, manager.getQueue().peek());
+	}
+	
+	@Test
+	public void queueTest() throws DaoException
+	{
+		testNotification2 = new Notification("title2", " message.",
+				Unit.MINUTES, 20 ,null,null);
+		
+		manager.getQueue().add(testNotification2);
+		assertEquals(manager.getQueue().size(),1);	
+		assertEquals(testNotification2, manager.getQueue().peek());
+		
+		manager.getQueue().remove(testNotification2);
+		assertTrue(manager.getQueue().size()==0);
+		assertEquals(testNotification1, manager.getQueue().peek());
+	
+	}
+	
 }
